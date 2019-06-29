5b0f2c7dc893fc6357d75e4c4a0a230094a1ce9f
Test file comit <>
2019-06-29 11:13:25 +0900

    Add seperate file

---
+++ src/main/java/com/kordic/cch/controller/HomeController.java
@@ -0,0 +1,231 @@
+package com.kordic.cch.controller;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.music.completion.vo.BoolVO;
+import com.music.completion.vo.DocVO;
+import com.music.completion.vo.PrefixVO;
+import com.music.completion.vo.QueryVO;
+import com.music.completion.vo.ResVO;
+import com.music.completion.vo.SearchVO;
+import com.music.completion.vo.ShouldVO;
+import com.music.completion.vo.TermVO;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+//	{"query":
+//	{"bool":
+//	{"should":[{"prefix":{"title":"노래"}},
+//	           {"term":{"titleNgram":"노래"}},
+//	           {"term":{"titleNgramEdge":"노래"}},
+//	           {"term":{"titleNgramEdgeBack":"노래"}}],
+//		"minimum_should_match":1}
+//	}
+//	}
+	
+	@RequestMapping(value="/search", produces="application/json;charset=UTF-8", method = RequestMethod.POST)
+	public @ResponseBody String search(SearchVO inVO){
+		System.out.println(inVO.toString());
+		String rtnStr = "";
+		String jsonStr = makeJsonStr(inVO.getSchWord());
+		
+//		private void callElasticApi(String method, String url, Object obj, String jsonData) {
+		try {
+			rtnStr = callElasticApi("GET", "/music_title/_search", null, jsonStr);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	
+		return rtnStr;
+	}
+	
+	private String makeJsonStr(String schWord) {
+		TermVO termNg = new TermVO();
+		TermVO termNgE = new TermVO();
+		TermVO termNgEb = new TermVO();
+		PrefixVO prefix = new PrefixVO();
+		termNg.setTitleNgram(schWord);
+		termNgE.setTitleNgramEdge(schWord);
+		termNgEb.setTitleNgramEdgeBack(schWord);
+		prefix.setTitle(schWord);
+		ShouldVO sdVO = null;
+		List<ShouldVO> shList = new ArrayList<ShouldVO>();
+		sdVO = new ShouldVO();
+		sdVO.setPrefix(prefix);		
+		shList.add(sdVO);
+		
+		sdVO = new ShouldVO();
+		sdVO.setTerm(termNg);
+		shList.add(sdVO);
+		
+		sdVO = new ShouldVO();
+		sdVO.setTerm(termNgE);
+		shList.add(sdVO);
+		
+		sdVO = new ShouldVO();
+		sdVO.setTerm(termNgEb);
+		shList.add(sdVO);
+		
+		BoolVO bVO = new BoolVO();
+		bVO.setShould(shList);
+		bVO.setMinimum_should_match(1);
+		
+		DocVO docVO = new DocVO();
+		QueryVO qVO = new QueryVO();
+		qVO.setBool(bVO);
+		docVO.setQuery(qVO);		
+		
+		Gson gson = new Gson();
+		String jsonStr = gson.toJson(docVO);
+
+		
+		System.out.println(jsonStr);
+		return jsonStr;
+	}
+	
+	private String callElasticApi(String method, String url, Object obj, String jsonData) throws Exception {
+		String host = "13.125.238.20";
+		int port = 9200;
+		try{
+            //엘라스틱서치에서 제공하는 response 객체
+            Response response = null;
+            String jsonString;
+            Gson gson = null;
+            if(null != jsonData) {
+            	jsonString = jsonData;
+            }else {
+            	
+            	gson = new Gson();
+                jsonString = gson.toJson(obj);
+            }
+            System.out.println("jsonString : " +jsonString);
+            RestClient restClient = RestClient.builder(
+            	    new HttpHost(host, port, "http")).build();
+            Request request = new Request(method, url);
+            request.addParameter("pretty", "true");
+            request.setEntity(new NStringEntity(jsonString, ContentType.APPLICATION_JSON));
+            
+            response = restClient.performRequest(request);
+            
+            
+//            curl -XGET "http://localhost:9200/sch_lyrics/_analyze" -H 'Content-Type: application/json' -d'
+//            {
+//              "analyzer": "lyric_analyzer",
+//              "text" :"진정인"
+//            }'
+            //앨라스틱서치에서 리턴되는 응답코드를 받는다
+            int statusCode = response.getStatusLine().getStatusCode();
+            System.out.println("status Code : " + statusCode);
+            //엘라스틱서치에서 리턴되는 응답메시지를 받는다
+            String responseBody = EntityUtils.toString(response.getEntity());
+            System.out.println("response Body : " + responseBody);
+            
+            restClient.close();
+            JsonParser parser = new JsonParser();
+            JsonElement rootObject = parser.parse(responseBody)
+            .getAsJsonObject().get("hits")
+            .getAsJsonObject().get("hits");
+            
+            JsonArray jarr = rootObject.getAsJsonArray();
+            System.out.println(jarr.size());
+            JsonObject jHitData = null;
+            JsonObject jData = null;
+            ResVO rtn = null;
+            List<ResVO> list = new ArrayList<ResVO>();
+            List<String> dupChkList = new ArrayList<String>();
+            
+            for(int i=0; i<jarr.size(); i++) {
+            	rtn = new ResVO();
+            	jHitData = jarr.get(i).getAsJsonObject();
+            	jData = jHitData.getAsJsonObject("_source");
+            	System.out.println(jData.get("musicTitle").getAsString());
+            	dupChkList.add(jData.get("musicTitle").getAsString());
+            }
+            
+            		// 2. 중복제거
+            List<String> uniqueList = this.removeDup(dupChkList);
+            
+            for(String str : uniqueList) {
+            	rtn = new ResVO();
+            	rtn.setMusicTitle(str);
+            	list.add(rtn);
+            }
+        	
+            gson = new Gson();
+            String rtnStr = gson.toJson(list);
+            System.out.println("rtnStr " + rtnStr);
+            return rtnStr;
+
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+		return null;
+	}
+	
+	
+	private List<String> removeDup(List<String> dataList) {
+		List<String> resultList = null;
+		int x = 0;
+		while(x < 1000) {
+			long starttime = System.nanoTime();
+			
+			resultList = new ArrayList<String>();
+			for(int i=0; i < dataList.size(); i++) {
+				if(!resultList.contains(dataList.get(i))) {
+					resultList.add(dataList.get(i));
+				}
+			}
+			long endtime = System.nanoTime();
+			long estimatedTime = endtime - starttime;
+			System.out.println(estimatedTime);
+			x++;
+		}
+		return resultList;
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/chhome.jsp
@@ -0,0 +1,162 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false" language="java"
+	contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<head>
+<title>Korean Dirctionary Search by Elasticsearch</title>
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<script>
+	/* Jquery autocomplete */
+	var availableWord = new Array();
+/*	$( function() {
+		$("#schWord").autocomplete({
+			source: availableWord
+		});
+	});
+*/
+	/* 검색어 조회 function */ 
+	/*function searchCompletion(){
+		var jsonStr = makeJsonString();
+        $.ajax({
+            url: '/completion/search',
+            type: 'POST',
+            // contentType: 'application/json; charset=UTF-8',
+            // contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            crossDomain: true,
+            dataType: 'json',
+            data: "schWord="+$('#schWord').val(),
+            success: function(response) {
+                console.log(response);
+                var obj = JSON.stringify(response);
+                var objJson = JSON.parse(obj);
+                var list = objJson.rtnList;
+                if(availableWord.length > 0){
+                	delete availableWord[0];
+            	}
+                for(var i=0; i<list.length; i++){
+                	console.log(list[i]);
+                	availableWord.push(list[i]);
+                }
+        		$("#schWord").autocomplete({
+        			source: availableWord
+        		});
+            },
+            error: function(status){
+            	console.log(status);
+            }
+        });
+	}*/
+	$(function() {
+		$("#schWord").autocomplete({
+			source: function (request, response){
+				$.ajax({
+					url: '/completion/search',
+		            type: 'POST',
+		            dataType: 'json',
+		            data: "schWord="+$('#schWord').val(),
+		            success: function(data) {
+		                /*console.log(data);
+		                var obj = JSON.stringify(data);
+		                var objJson = JSON.parse(obj);
+		                var list = objJson.rtnList;
+		                if(availableWord.length > 0){
+		                	delete availableWord[0];
+		            	}*/
+		            	response(
+	            			$.map(data, function (item){
+	            				var obj = JSON.stringify(data);
+	    		                var objJson = JSON.parse(obj);
+	    		                var list = objJson.rtnList;
+	            				return {	            					
+	            						value: item.musicTitle,
+				                		label: item.musicTitle
+	            				}
+		                	})
+		                );
+		                
+		                /*for(var i=0; i<list.length; i++){
+		                	console.log(list[i]);
+		                	availableWord.push(list[i]);
+		                }
+		        		$("#schWord").autocomplete({
+		        			source: availableWord
+		        		});
+		                */
+		            }     
+				})
+			},
+			open: function() {
+	            $( this ).autocomplete("widget").width("323px");
+	            $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+	        }
+		})
+		.data('uiAutocomplete')._renderItem = function( ul, item ) {
+        return $( "<li style='cursor:hand; cursor:pointer;'></li>" )
+            .data( "item.autocomplete", item )
+            .append("<a onclick=\"cityValue('" + item.value + "');\">"  + unescape(item.label) + "</a>")
+        .appendTo( ul );
+	    };
+
+
+	});
+	
+	
+	function makeJsonString(){
+		 var docJson = new Object();
+         var query = new Object();
+         var bool = new Object();
+         var should = new Array();
+         var shouldPrefix = new Object();
+         var shouldTerm1 = new Object();
+         var shouldTerm2 = new Object();
+         var shouldTerm3 = new Object();
+         var prefix = new Object();
+         
+         
+         var term = new Object();
+         term.titleNgram = "노래";
+         shouldTerm1.term = term;
+		 term = new Object();
+		 term.titleNgramEdge = "노래";
+		 shouldTerm2.term = term;
+		 term = new Object();
+		 term.titleNgramEdgeBack = "노래";
+		 shouldTerm3.term = term;
+		 prefix.title = "노래";
+		 shouldPrefix.prefix = prefix;
+         
+         should.push(shouldPrefix);
+         should.push(shouldTerm1);
+         should.push(shouldTerm2);
+         should.push(shouldTerm3);
+         
+         bool.should = should;
+         bool.minimum_should_match = 1;
+         query.bool = bool;
+		 
+         docJson.query = query;
+         
+         var jsonInfo = JSON.stringify(docJson);
+         console.log(jsonInfo); //브라우저 f12개발자 모드에서 confole로 확인 가능
+         return jsonInfo;
+	}
+	</script>
+	<script type="text/javascript">
+
+</script>
+
+</head>
+<body>
+	<h1>Music Title Search AutoCompletion</h1>	
+	<div class="ui-widget">
+		<label for="schWord">Title : </label>
+		<input id="schWord" type="text"/>
+	</div>
+
+
+</body>
+</html>
