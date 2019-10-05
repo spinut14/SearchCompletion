7c4f48c9c4d4774bfc75c83f6ff0a87c846ae654
Test file comit <>
2019-10-05 22:58:59 +0900

    Source Remark for build ( Junit Test File)

--- src/test/java/com/kordic/kdy/controller/SearchControllerTest.java
+++ src/test/java/com/kordic/kdy/controller/SearchControllerTest.java
@@ -1,11 +1,6 @@
 package com.kordic.kdy.controller;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.kordic.kdy.vo.SearchWord;
 
 //@RunWith(SpringJunit4ClassRunner.class)
 public class SearchControllerTest {
@@ -14,11 +9,11 @@ public class SearchControllerTest {
 	@Autowired
 	private HomeKDYController kdycontroller;
 	
-	@Test
-	public void test() throws Exception {
-		System.out.println("test");
-		SearchWord sw = new SearchWord("의자");
-		kdycontroller.searchDictionary(sw);
-		
-	}
+//	@Test
+//	public void test() throws Exception {
+//		System.out.println("test");
+//		SearchWord sw = new SearchWord("의자");
+//		kdycontroller.searchDictionary(sw);
+//		
+//	}
 }
